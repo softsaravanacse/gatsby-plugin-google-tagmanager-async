5c04b24f2b13b0112f17264a2fa0577f5c5d114b
Saravana Kumar <softsaravanacse@gmail.com>
2020-03-20 18:41:57 +0530

    Moved the script inside setTimeout function

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -9,13 +9,14 @@ const generateGTM = ({
   environmentParamStr,
   dataLayerName,
   timeout
-}) => stripIndent `  
-  var gtmScript=function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl+'${environmentParamStr}';f.parentNode.insertBefore(j,f);
-  };
-  setTimeout(function(){gtmScript(window,document,'script','${dataLayerName}', '${id}');},${timeout | 1000});
+}) => stripIndent `    
+  setTimeout(function(){
+      var gtmScript=function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl+'${environmentParamStr}';f.parentNode.insertBefore(j,f);
+    };
+    gtmScript(window,document,'script','${dataLayerName}', '${id}');},${timeout | 1000});
   `
 
 const generateGTMIframe = ({
